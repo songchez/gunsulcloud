8703c1f208d2ced004553a878dd049a98fe2f011
songchez <tama4840@gmail.com>
2023-01-10 08:05:30 +0900

    gogo

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
-  locale: 'en',
+  site_name: '테스트',
+  title: '넥스트+테일윈드',
+  description: '설명입니다다다ㅏ다다다다ㅏ다ㅏ',
+  locale: 'kr',
 };
