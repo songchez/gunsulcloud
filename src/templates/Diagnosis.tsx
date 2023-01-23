78226d8c816848d159834dce7956e7b4276cfe57
songchez <tama4840@gmail.com>
2023-01-23 23:43:09 +0900

    successssss

--- src/templates/Diagnosis.tsx
+++ src/templates/Diagnosis.tsx
@@ -1,22 +1,34 @@
-import { TextField } from '@mui/material';
+import { Button, Container, TextField } from '@mui/material';
 
 import { Section } from '../layout/Section';
 
 export default function Diagnosis() {
   return (
-    <div>
+    <div className="flex-row justify-center w-full p-4">
       <Section>
-        <div className="text-center flex flex-col p-4 sm:text-left sm:flex-row sm:items-center sm:justify-between sm:p-12 bg-primary-100 rounded-md">
+        <div className="flex flex-col justify-center gap-4">
           <div className="text-2xl font-semibold">
             <div className="text-primary-500">기업진단</div>
           </div>
-
-          <div className="flex justify-center">
-            <TextField
-              id="sangho"
-              label="상호명"
-              variant="outlined"
-            ></TextField>
+          <TextField id="sangho" label="상호명" variant="outlined"></TextField>
+          <TextField
+            id="jabongum"
+            label="자본금"
+            variant="outlined"
+          ></TextField>
+          <TextField id="dugazza" label="상태_1" variant="outlined"></TextField>
+          <TextField
+            id="dugazza"
+            label="기술인력"
+            variant="outlined"
+          ></TextField>
+          <TextField id="dugazza" label="상태_2" variant="outlined"></TextField>
+          <div className="flex-col justify-center m-5">
+            <Container>
+              <Button variant="outlined" size="large">
+                기업진단받기
+              </Button>
+            </Container>
           </div>
         </div>
       </Section>
