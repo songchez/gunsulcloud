e20e284ccd2c4a6da3ff0e4c401ce55646af3ef2
songchez <tama4840@email.com>
2023-01-24 10:08:04 +0900

    layout

--- src/templates/Diagnosis.tsx
+++ src/templates/Diagnosis.tsx
@@ -1,37 +1,26 @@
-import { Button, Container, TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 
-import { Section } from '../layout/Section';
+import MultipleSelectChip from './tests';
 
 export default function Diagnosis() {
   return (
-    <div className="flex-row justify-center w-full p-4">
-      <Section>
-        <div className="flex flex-col justify-center gap-4">
-          <div className="text-2xl font-semibold">
-            <div className="text-primary-500">기업진단</div>
-          </div>
-          <TextField id="sangho" label="상호명" variant="outlined"></TextField>
-          <TextField
-            id="jabongum"
-            label="자본금"
-            variant="outlined"
-          ></TextField>
-          <TextField id="dugazza" label="상태_1" variant="outlined"></TextField>
-          <TextField
-            id="dugazza"
-            label="기술인력"
-            variant="outlined"
-          ></TextField>
-          <TextField id="dugazza" label="상태_2" variant="outlined"></TextField>
-          <div className="flex-col justify-center m-5">
-            <Container>
-              <Button variant="outlined" size="large">
-                기업진단받기
-              </Button>
-            </Container>
-          </div>
+    <div className="flex justify-center p-4 bg-primary-100 m-4 rounded-lg">
+      <div className="flex flex-col justify-center gap-4 w-screen max-w-xl">
+        <div className="text-2xl font-semibold">
+          <div className="text-primary-500">기업진단</div>
         </div>
-      </Section>
+        <MultipleSelectChip></MultipleSelectChip>
+        <TextField id="sangho" label="상호명" variant="outlined"></TextField>
+        <TextField id="jabongum" label="자본금" variant="outlined"></TextField>
+        <TextField id="dugazza" label="상태_1" variant="outlined"></TextField>
+        <TextField id="dugazza" label="기술인력" variant="outlined"></TextField>
+        <TextField id="dugazza" label="상태_2" variant="outlined"></TextField>
+        <div className="flex justify-center m-5">
+          <Button variant="outlined" size="large">
+            기업진단받기
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
