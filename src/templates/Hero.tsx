b79d16bb7edc4b605ca8f269300bcf6d59e75b76
songchez <tama4840@email.com>
2023-02-01 13:25:48 +0900

    backimagee

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -6,16 +6,19 @@ import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
 
 const Hero = () => (
-  <Background color="bg-gray-100">
-    <Section yPadding="pt-20 pb-32">
+  <Background>
+    <Section yPadding="pt-1 pb-32">
       <HeroOneButton
         title={
           <>
-            {'신규건설업등록, 이제 고민하지마세요\n'}
+            {'신규건설업등록,'}
+            <br></br>
+            <span>이제 고민하지마세요</span>
+            <br></br>
             <span className="text-primary-500">건설왕에게 맡기세요</span>
           </>
         }
-        description={`신규건설업 등록, 아직도 고민 중이신가요? \n 여러분도 할 수 있습니다.`}
+        description={`신규건설업 등록, 아직도 고민 중이신가요? 여러분도 할 수 있습니다.`}
         button={
           <Link href="/">
             <a>
