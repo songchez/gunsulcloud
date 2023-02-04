dd116bdd060ddc0a79db79df73b6faf758052df1
songchez <tama4840@gmail.com>
2023-02-04 16:07:33 +0900

    submit

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from 'firebase/firestore/lite';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDCCqas6UrTBlr2afb74sHchnfdCAw9PQ',
-  authDomain: 'gunsul-77884.firebaseapp.com',
-  projectId: 'gunsul-77884',
-  storageBucket: 'gunsul-77884.appspot.com',
-  messagingSenderId: '68312113616',
-  appId: '1:68312113616:web:4d11f69477fed8875d99d5',
-  measurementId: 'G-98FRC6QL8P',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
