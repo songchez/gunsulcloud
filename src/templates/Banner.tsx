72f23ff35a8b92eea358153f6bbf70831b3e2e0a
songchez <tama4840@gmail.com>
2023-09-11 11:25:33 +0900

    삭제할거 삭제

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -1,7 +1,6 @@
-import { Button } from '../components/button/Button';
-import { CTABanner } from '../components/cta/CTABanner';
-import { PhoneBanner } from '../components/cta/PhoneBanner';
-import { Section } from '../components/layout/Section';
+import { CTABanner } from "../components/cta/CTABanner";
+import { PhoneBanner } from "../components/cta/PhoneBanner";
+import { Section } from "../components/layout/Section";
 
 const Banner = () => (
   <Section>
@@ -9,8 +8,8 @@ const Banner = () => (
       title="잠깐!"
       subtitle="전문가에게 더 상세한 진단을 받아보세요"
       button={
-        <a href="http://pf.kakao.com/_xnxkBexj/chat">
-          <Button text="신청하기" />
+        <a className="btn" href="http://pf.kakao.com/_xnxkBexj/chat">
+          신청하기
         </a>
       }
     />
--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/button/Button";
 import { Background } from "../components/background/Background";
 import { HeroOneButton } from "../components/hero/HeroOneButton";
 
@@ -19,8 +18,8 @@ const Hero = () => (
       }
       description={`언제나 건설클라우드를 사랑해 주셔서 감사합니다.`}
       button={
-        <a href="http://pf.kakao.com/_xnxkBexj/chat">
-          <Button text="건설업등록신청" />
+        <a className="btn" href="http://pf.kakao.com/_xnxkBexj/chat">
+          건설업등록신청
         </a>
       }
     />
